c079c039e47f87df4e0aa8044056c5bf09f77840
Angel Martínez-Tenor <angelmtenor@gmail.com>
2017-05-19 12:29:58 +0200

    feat: convolutional neural network for image classification

---
+++ L2_Convolutional_Neural_Networks/B_cnn_for_image_classification.py
@@ -0,0 +1,16 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+model = Sequential()
+model.add(Conv2D(filters=16, kernel_size=2, padding='same', activation='relu', input_shape=(32, 32, 3)))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=32, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Flatten())
+model.add(Dense(500, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(10, activation='softmax'))
+
+model.summary()
